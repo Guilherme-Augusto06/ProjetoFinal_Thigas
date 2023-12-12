34382dde2034dd7e36a4f6b3cef88f42ec72855d
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2023-12-12 15:58:52 -0300

    Add files via upload

--- pages/Painel.jsx
+++ pages/Painel.jsx
@@ -1,13 +1,15 @@
 import Header from "../components/Header"
 import Conteudo from "../components/Conteudo"
+import Tabela from "../components/Tabela"
 
-function Painel () {
+function Painel(){
     return(
         <>
-        <Header btn={true}/>
-        <Conteudo>
-            <h2>Lista de Funcionarios</h2>
-        </Conteudo>
+            <Header btn={true}/>
+            <Conteudo>
+                <h2>Lista de Funcionários</h2>
+                <Tabela />
+            </Conteudo>
         </>
     )
 }
